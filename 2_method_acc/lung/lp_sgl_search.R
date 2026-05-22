66e717271a93ff896bbfac474bddb3d9cdbff970
Exceret <15364051195@163.com>
2026-05-22 08:49:59 +0800

    update

--- 2_method_acc/lung/lp_sgl_search.R
+++ 2_method_acc/lung/lp_sgl_search.R
@@ -33,3 +33,63 @@ if (anyNA(bulk)) {
 if (anyNA(pheno_bi)) {
   stop("pheno_bi has NA")
 }
+
+source("../rlogunif.R")
+
+set.seed(123)
+arg_samples <- data.frame(
+  alpha = rlogunif(50, 0.005), # 第1维
+  resolution = sample(seq(0, 1, 0.01), 50, replace = TRUE),
+  nfold = sample(seq(5, 20, 1), 50, replace = TRUE)
+) %>%
+  dplyr::add_row(alpha = 0.5, resolution = 0.6, nfold = 5) # default parameters
+
+
+# * run LP_SGL with error handling
+res_list <- lapply(
+  seq_len(nrow(arg_samples)),
+  function(i) {
+    cli::cli_h1("{i} / {nrow(arg_samples)}")
+    result <- SigBridgeR::Screen(
+      matched_bulk = bulk,
+      sc_data = sc_data,
+      phenotype = pheno_bi,
+      label_type = glue::glue("process_{i}"),
+      phenotype_class = "binary",
+      screen_method = "LP_SGL",
+      alpha = arg_samples$alpha[i], # select_alpha will be used
+      resolution = arg_samples$resolution[i],
+      nfold = as.integer(arg_samples$nfold[i])
+    )
+
+    # qs::qsave(
+    #     scab_result,
+    #     file = glue::glue("scAB_results/scab_result_{i}.qs")
+    # )
+
+    data <- data.frame(
+      pos_cell = (result$scRNA_data$LP_SGL == "Positive")
+    )
+    colnames(data) = glue::glue("process_{i}")
+
+    gc(verbose = FALSE)
+
+    # 返回包含索引和结果的数据框
+    return(data)
+  }
+)
+
+
+# 合并所有结果
+all_results <- do.call(cbind, res_list)
+rownames(all_results) = colnames(seurat) # each cell is a row
+
+data.table::fwrite(
+  all_results,
+  file = "stats/lp_sgl_label_mat1.csv",
+  row.names = TRUE
+)
+
+cli::cli_alert_success(crayon::green("(1)lpsgl random search completed."))
+
+# ! TCGA_LUAD
