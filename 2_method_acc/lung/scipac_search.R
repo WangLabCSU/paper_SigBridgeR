3f506ff733fbe6301107af56e2b7e6c43053f714
Exceret <15364051195@163.com>
2026-08-09 11:52:17 +0800

    update

--- 2_method_acc/lung/scipac_search.R
+++ 2_method_acc/lung/scipac_search.R
@@ -12,9 +12,9 @@ data_path <- "/home/data/sigbridger/benchmark_data/lung"
 sc_data <- qs::qread(file.path(data_path, "luad_GSE123902_seurat.qs"))
 
 bulk <- qs::qread(
-  file.path(data_path, "TCGA_LUAD_bulkdata.qs")
+  file.path(data_path, "TCGA_LUAD_bulkdata_tpm.qs")
 )
-bulk <- log2(bulk + 1)
+
 
 pheno <- qs::qread(file.path(data_path, "TCGA_LUAD_pheno.qs"))
 
@@ -24,7 +24,10 @@ pheno_bi <- mutate(pheno, sample_type = substr(pheno$sample, 14, 15)) %>%
   mutate(sample_type = as.integer(sample_type == "01"))
 pheno_bi <- setNames(pheno_bi$sample_type, pheno_bi$sample)
 
-bulk <- bulk[, names(pheno_bi)]
+cm_samples <- intersect(colnames(bulk), names(pheno_bi))
+
+bulk <- bulk[, cm_samples]
+pheno_bi <- pheno_bi[cm_samples]
 
 if (!all(colnames(bulk) == names(pheno_bi))) {
   stop("bulk and pheno_bi not match")
